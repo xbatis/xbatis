2dfc36e9a0a1d2b5ae71f7403b6e2bd34722699d
Ai东 <abc-127@live.cn>
2025-02-14 09:57:06 +0800

    修复lambda 识别前2个字符都是大写的字段错误的问题

--- mybatis-mp-sql-api/src/main/java/db/sql/api/tookit/PropertyNamer.java
+++ mybatis-mp-sql-api/src/main/java/db/sql/api/tookit/PropertyNamer.java
@@ -27,11 +27,10 @@ public class PropertyNamer {
             if (!name.startsWith("get") && !name.startsWith("set")) {
                 throw new RuntimeException("Error parsing property name '" + name + "'.  Didn't start with 'is', 'get' or 'set'.");
             }
-
             name = name.substring(3);
         }
 
-        if (name.length() == 1 || name.length() > 1 && !Character.isUpperCase(name.charAt(1))) {
+        if (name.length() >= 1) {
             name = name.substring(0, 1).toLowerCase(Locale.ENGLISH) + name.substring(1);
         }
 
