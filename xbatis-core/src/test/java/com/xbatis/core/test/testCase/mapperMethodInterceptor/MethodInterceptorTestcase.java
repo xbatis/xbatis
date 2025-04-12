a18f3d16e898c0b9d744073c2cc8f8cd2b677092
Ai东 <abc-127@live.cn>
2025-04-12 10:22:39 +0800

    优化测试用例

--- xbatis-core/src/test/java/com/xbatis/core/test/testCase/mapperMethodInterceptor/MethodInterceptorTestcase.java
+++ xbatis-core/src/test/java/com/xbatis/core/test/testCase/mapperMethodInterceptor/MethodInterceptorTestcase.java
@@ -18,14 +18,45 @@ public class MethodInterceptorTestcase extends BaseTest {
             XbatisGlobalConfig.addMapperMethodInterceptor(new MyMapperMethodInterceptor());
             XbatisGlobalConfig.addMapperMethodInterceptor(new MyMapperMethodInterceptor2());
             Integer cnt;
-//             cnt = sysUserMapper.nativeCount();
-//            assertEquals(cnt, 3);
+            cnt = sysUserMapper.nativeCount();
+            assertEquals(cnt, 3);
 //
             cnt = sysUserMapper.javaCount();
             assertEquals(cnt, 3);
 
-//            cnt = sysUserMapper.javaLimitAnnotationCount();
-//            assertEquals(123,cnt );
+            cnt = sysUserMapper.javaLimitAnnotationCount();
+            assertEquals(123, cnt);
+        }
+    }
+
+    @Test
+    public void test2() {
+        try (SqlSession session = this.sqlSessionFactory.openSession(false)) {
+            SysUserMapper sysUserMapper = session.getMapper(SysUserMapper.class);
+
+            XbatisGlobalConfig.removeMapperMethodInterceptor(MyMapperMethodInterceptor2.class);
+            XbatisGlobalConfig.addMapperMethodInterceptor(new MyMapperMethodInterceptor());
+
+            Integer cnt;
+
+            cnt = sysUserMapper.javaLimitAnnotationCount();
+            assertEquals(3, cnt);
+        }
+    }
+
+    @Test
+    public void test3() {
+        try (SqlSession session = this.sqlSessionFactory.openSession(false)) {
+            SysUserMapper sysUserMapper = session.getMapper(SysUserMapper.class);
+            XbatisGlobalConfig.removeMapperMethodInterceptor(MyMapperMethodInterceptor.class);
+            XbatisGlobalConfig.addMapperMethodInterceptor(new MyMapperMethodInterceptor2());
+
+            Integer cnt;
+
+            cnt = sysUserMapper.javaLimitAnnotationCount();
+            assertEquals(123, cnt);
         }
     }
 }
+
+
