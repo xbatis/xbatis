2f8a0432ff884f0c140a63e478685667a692e0cd
Ai东 <abc-127@live.cn>
2025-04-27 17:33:05 +0800

    修复notExists双重括号

--- xbatis-sql-api-impl/src/main/java/db/sql/api/impl/cmd/executor/AbstractSubQuery.java
+++ xbatis-sql-api-impl/src/main/java/db/sql/api/impl/cmd/executor/AbstractSubQuery.java
@@ -27,6 +27,7 @@ import db.sql.api.impl.cmd.basic.Table;
 import db.sql.api.impl.cmd.basic.TableField;
 import db.sql.api.impl.cmd.condition.Exists;
 import db.sql.api.impl.cmd.condition.In;
+import db.sql.api.impl.cmd.condition.NotExists;
 import db.sql.api.impl.cmd.struct.*;
 import db.sql.api.impl.cmd.struct.query.*;
 import db.sql.api.impl.tookit.Objects;
@@ -154,7 +155,7 @@ public abstract class AbstractSubQuery<SELF extends AbstractSubQuery<SELF, CMD_F
     @Override
     public StringBuilder sql(Cmd module, Cmd parent, SqlBuilderContext context, StringBuilder sqlBuilder) {
 
-        if (parent instanceof In || parent instanceof Exists || parent instanceof With) {
+        if (parent instanceof In || parent instanceof Exists || parent instanceof NotExists || parent instanceof With) {
             return super.sql(module, parent, context, sqlBuilder);
         }
         sqlBuilder.append(SqlConst.BRACKET_LEFT);
