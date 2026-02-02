748980398c29fecd39eeed192b2805a390a0af11
Ai东 <abc-127@live.cn>
2026-02-02 15:24:43 +0800

    修复多个主键批量修改异常问题

--- xbatis-core/src/main/java/cn/xbatis/core/db/reflect/TableInfo.java
+++ xbatis-core/src/main/java/cn/xbatis/core/db/reflect/TableInfo.java
@@ -140,7 +140,7 @@ public class TableInfo {
             this.schemaAndTableName = schema + "." + tableName;
         }
 
-        TableFieldInfo idFieldInfo = null;
+
         TableFieldInfo versionFieldInfo = null;
         TableFieldInfo tenantIdFieldInfo = null;
         TableFieldInfo logicDeleteFieldInfo = null;
@@ -149,7 +149,7 @@ public class TableInfo {
         List<TableFieldInfo> tableFieldInfos = new ArrayList<>();
         Map<String, TableFieldInfo> tableFieldInfoMap = new HashMap<>();
         Map<Class<?>, ForeignInfo> foreignInfoMap = new HashMap<>();
-        boolean hasMutilId = false;
+
 
         List<TableFieldInfo> idFieldInfos = new ArrayList<>(6);
 
@@ -168,13 +168,8 @@ public class TableInfo {
 
             if (tableFieldInfo.isTableId()) {
                 idFieldInfos.add(tableFieldInfo);
-                if (Objects.isNull(idFieldInfo)) {
-                    idFieldInfo = tableFieldInfo;
-                } else {
-                    idFieldInfo = null;
-                    hasMutilId = true;
-                }
             }
+
             if (tableFieldInfo.isVersion()) {
                 if (versionFieldInfo != null) {
                     throw new RuntimeException("Entity " + entity.getName() + " has multi @Version");
@@ -204,6 +199,13 @@ public class TableInfo {
                 logicDeleteTimeFieldInfo = tableFieldInfo;
             }
         }
+
+        TableFieldInfo idFieldInfo = null;
+        if (idFieldInfos.size() == 1) {
+            idFieldInfo = idFieldInfos.get(0);
+        }
+
+        boolean hasMutilId = idFieldInfos.size() > 1;
 
         this.tableFieldInfos = Collections.unmodifiableList(tableFieldInfos);
         this.fieldSize = this.tableFieldInfos.size();
